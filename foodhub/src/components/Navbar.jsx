a2d0608ec26961a19fd8d67a34066f0640ab219a
MumtahinaRahman <vanallope2015@gmail.com>
2023-02-02 22:32:07 +0600

    navbar coming soon

---
+++ foodhub/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+    return ( 
+        <div className="navbar">
+            <a href="#!" className="logo">Food<span>Hub</span></a>
+            <div className="nav-links">
+                <a href="#!">Home</a>
+                <a href="#!">Recipes</a>
+                <a href="#!">Settings</a>
+            </div>
+
+        </div>
+     );
+}
+
+export default Navbar;
