387adcf7dbd9f6761d36a98d4091d79ce28133a7
MumtahinaRahman <vanallope2015@gmail.com>
2023-04-09 11:49:37 +0600

    navbar

--- foodhub/src/components/Navbar.jsx
+++ foodhub/src/components/Navbar.jsx
@@ -1,13 +1,13 @@
+import '../styles/index.scss';
+
 function Navbar() {
     return ( 
-        <div className="navbar">
+        <div className="navbar container">
             <a href="#!" className="logo">Food<span>Hub</span></a>
             <div className="nav-links">
                 <a href="#!">Home</a>
                 <a href="#!">Recipes</a>
                 <a href="#!">Settings</a>
-            </div>
-
         </div>
      );
 }
